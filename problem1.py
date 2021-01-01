42098830678ed98210bc37a1b33793809c497b5c
hayleycd <cook.hayley@gmail.com>
2021-01-01 13:34:50 -0800

    Reformatted with black

--- problem1.py
+++ problem1.py
@@ -1,11 +1,13 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+
 def sum_multiples(term1, term2, limit):
     multiples = []
     for num in range(limit):
         if num % term1 == 0 or num % term2 == 0:
             multiples.append(num)
     return sum(multiples)
+
 
 # Answer: 233168
--- problem3.py
+++ problem3.py
@@ -1,14 +1,16 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+
 def largest_prime_factor(number_to_check):
-    for potential_factor in range(1, number_to_check//2):
+    for potential_factor in range(1, number_to_check // 2):
         if number_to_check % potential_factor == 0:
             factor_pair = number_to_check // potential_factor
             is_prime = is_prime(factor_pair)
             if is_prime:
-                 return factor_pair
+                return factor_pair
+
 
 def is_prime(factor_to_check):
-    # to do: write a function to determine if a number is prime. 
+    # to do: write a function to determine if a number is prime.
     return True
